5139e27d070580ee6898eb53be46ea1806e8c64a
Michael Wales <michaelwales@gmail.com>
2020-05-25 17:02:37 +0100

    Creates working Add Method in Calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before() {
+        calculator = new Calculator(28, 6);
+    }
+
+    @Test
+    public void canAdd() {
+        assertEquals(34, calculator.add());
+    }
 
 }
